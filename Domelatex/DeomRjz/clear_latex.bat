57cec6dfaa3f6ac5990c23972ce8a90a7dda010f
JianzeRenP1 <2572020176@qq.comP1>
2024-07-15 00:00:09 +0800

    原始提交：DemoRjz初版

---
+++ Domelatex/DeomRjz/clear_latex.bat
@@ -0,0 +1,21 @@
+del *.aux  
+del *.bcf 
+del *.nlo
+del *.synctex(busy)
+del *.blg 
+del *.fdb_latexmk 
+del *.fls 
+del *.toc
+del *.log 
+del *.xml 
+del *.lot 
+del *.lof 
+del *.xdv 
+del *.thm
+del *.out
+del *.gz
+del *.spl
+del *.bbl
+del *.cpt
+
+exit
